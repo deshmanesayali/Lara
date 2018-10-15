6ac124825a51b6440bd06b2d4e67d35db44b1925
Sayali Deshmane <sd796@njit.edu>
2018-10-15 14:23:22 -0700

    task: created about page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.sidebar')
+@section('content')
+    <div class="container">
+        <p>Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.</p>
+    </div>
+@stop
