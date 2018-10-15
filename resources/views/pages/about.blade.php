dbbc8abf080e2bf237f5455a8639ea383a527cc5
Sayali Deshmane <sd796@njit.edu>
2018-10-15 14:31:27 -0700

    fix: made changes in about.php as error 404 was shown

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.sidebar')
+@extends('layouts.default')
 @section('content')
     <div class="container">
         <p>Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.</p>
