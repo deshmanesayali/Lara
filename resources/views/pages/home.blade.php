37c7ea09c52314b8956057b0685ff3de3e3caf86
Sayali Deshmane <sd796@njit.edu>
2018-10-15 14:20:26 -0700

    task: created homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+    <div class="container">
+  <h1>  Welcome to world of Laravel!</h1>
+    <h2><a href="#">Follow us on Facebook</a></h2></div>
+
+@stop
