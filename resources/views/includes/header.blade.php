326038bc5ac6fe62e0b7b099a62c04573090db96
Sayali Deshmane <sd796@njit.edu>
2018-10-15 14:11:08 -0700

    task: added header.blade.php

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,10 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">Laravel</a>
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+
+        </ul>
+    </div>
+</div>
